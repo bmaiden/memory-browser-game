8db7e0949eba206c9101a3027f2a6f20566740db
bmaiden <b.maiden2012@yahoo.com>
2023-11-29 14:13:51 -0600

    adds comment headings and high-level objective to JS

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+// 1) Display an empty game board when the page is initially displayed.
+// 2) A player can click on sixteen cells to make a move.
+// 3) Every two clicks will determine if there is a match.
+// 4) Once a match has been determined, the cells cannot be played again.
+// 5) Set a time limit to be met
+// 6) Send message if won or loss
+// 7) Provide a Reset Game button that will clear the contents of the board.
+
 //Testing to make sure HTML & CSS are linked to JS
 console.log("This is working!");
+
+/*----- constants -----*/
+
+
+
+/*----- state variables -----*/
+
+
+
+/*----- cached DOM elements  -----*/
+//save HTML elements as variables to use later
+
+
+
+/*----- functions -----*/
+
+
+
+/*---- event listeners -----*/
+
+
